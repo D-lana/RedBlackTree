fc08b5c3112e412f4ed81e54eb66c16c07d86051
Margarita <91904143+D-lana@users.noreply.github.com>
2022-05-27 20:16:41 +0300

    Update RedBlackTree.hpp

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -1,106 +1,204 @@
-#ifndef redBlackTree_HPP
-#define redBlackTree_HPP
+#ifndef REDBLACKTREE_HPP
+#define REDBLACKTREE_HPP
+
+#define RED_ "\x1b[1;31m"
+#define BLACK_ "\x1b[0m"
 
 #include <iostream>
 
-enum Color { Black, Red };
-
-template<class T>
-struct  Tree
-{
-	T data;
-	Color color;
-	Tree *left;
-	Tree *right;
-	Tree *parent;
+enum Color {
+	BLACK,
+	RED
 };
 
-template<class T>
+template <typename T>
+struct Tree
+{
+	T				data;
+	struct Tree		*left;
+	struct Tree		*right;
+	struct Tree		*parent;
+	Color			color;
+};
+
+template <typename T>
 class redBlackTree {
-    private:
-    Tree<T> *tree;
-    size_t count;
-    public:
-    
-    
-    redBlackTree() {
-       tree = NULL; 
-       count = 0;
-    }
-    
-    ~redBlackTree() {
-        
-    }
-    
-    void insert(const T& _data) {
-        if (tree == NULL)
-        {
-            tree = new Tree<T>;
-            tree->left = NULL;
-            tree->right = NULL;
-            tree->data = _data;
-            tree->color = Black;
-            count++;
-        }
-        else
-        {
-            Tree<T> *newtree;
-            newtree = new Tree<T>;
-            Tree<T> *tmp = tree;
-            newtree->right = NULL;
-            newtree->left = NULL;
-            newtree->data = _data;
-            newtree->color = Red;
-            lookForPlace(newtree, tmp, _data); 
-            if (newtree->parent->color == Red) {
-                if (newtree->parent->left == newtree) {
-                    rightRotate(newtree->parent);
-                }
-                else {
-                    leftRotate(newtree->parent);
-                }
-            }
-        }
-    }
-        
-    void lookForPlace(Tree<T> *newtree, Tree<T> *tmp, const T& _data)
-    {
-        if (_data == tmp->data) {
-            delete newtree;
-            return ;
-        }
-        else
-        {
-            if (_data < tmp->data)
-            {
-                if (tmp->left != NULL) {
-                    tmp = tmp->left;
-                    lookForPlace(newtree, tmp, _data);
-                }
-                else {
-                    tmp->left = newtree;
-                    newtree->parent = tmp;
-                    return ;
-                }
-            }
-            else if (_data > tmp->data)
-            {
-                if (tmp->right != NULL) {
-                    tmp = tmp->right;
-                    lookForPlace(newtree, tmp, _data);
-                }
-                else {
-                    tmp->right = newtree;
-                    newtree->parent = tmp;
-                    return ;
-                }
-            }
-            //coloration(newtree);
-        }
-    }
-    
-    void leftRotate(Tree<T> *rotate) {
-        std::cout << "leftRotate" << rotate->data << std::endl;
+	private:
+	int count;
+	Tree<T> *tree;
+	public:
+
+	redBlackTree() {
+		tree = NULL;
+		this->count = 0;
+	}
+
+	void deleteTree(Tree<T> *tree, const int level = 0)
+	{
+		if (tree->left) {
+			deleteTree(tree->left, level + 1);
+		}
+		delete tree;
+		if (tree->right) {
+			deleteTree(tree->right, level + 1);
+		}
+	}
+
+	~redBlackTree() {
+		deleteTree(tree, 0);
+	}
+
+	Tree<T> *getTree() {
+		return tree;
+	}
+
+	void findPlace(const T &data, Tree<T> *tmp, Tree<T> *newTree) {
+		while (tmp != NULL) {
+			if (data > tmp->data) {
+				if (tmp->right == NULL) {
+					//std::cout << "add: " << data << std::endl;
+					newTree->parent = tmp;
+					tmp->right = newTree;
+					redUncle(tmp);
+					break ;
+				}
+				tmp = tmp->right;
+			}
+			else if (data < tmp->data) {
+				if (tmp->left == NULL) {
+					//std::cout << "add: " << data << std::endl;
+					tmp->left = newTree;
+					newTree->parent = tmp;
+					redUncle(tmp);
+					break ;
+				}
+				tmp = tmp->left;
+			}
+			else {
+				//std::cout << "delete: " << data << std::endl;
+				delete newTree;
+				return ;
+			}
+		}
+	}
+
+	void redUncle(Tree<T> *tmp) {
+		if (tmp->parent != NULL && ((tmp->parent->right == tmp && tmp->parent->left && tmp->parent->left->color == RED) ||
+			(tmp->parent->left == tmp && tmp->parent->right && tmp->parent->right->color == RED))) {
+				tmp->parent->right->color = BLACK;
+				tmp->parent->left->color = BLACK;
+				tmp->parent->color = RED;
+				std::cout << "\x1b[1;31m redUncle \x1b[0m" << std::endl;
+			if (tmp->parent->parent != NULL) {
+				redUncle(tmp->parent->parent);
+			}
+			else if (tmp->parent->parent == NULL) {
+				tmp->parent->color = BLACK;
+			}
+		}
+	}
+
+	void rightRotateSmall(Tree<T> *parent, Tree<T> *child) {
+		std::cout << "rightRotateSmall: " << parent->data << std::endl;
+		Tree<T> *grandParent = parent->parent;
+		parent->parent = child;
+		child->parent = grandParent;
+		child->right = parent;
+		parent->left = NULL;
+		grandParent->right = child;
+	}
+
+	void leftRotateSmall(Tree<T> *parent, Tree<T> *child) {
+		std::cout << "leftRotateSmall: " << parent->data << std::endl;
+		Tree<T> *grandParent = parent->parent;
+		parent->parent = child;
+		child->parent = grandParent;
+		child->left = parent;
+		parent->right = NULL;
+		grandParent->left = child;
+	}
+
+
+
+	void blackUncle(Tree<T> *tmp, Tree<T> *child) {
+		if (tmp->parent != NULL && (tmp->parent->right == tmp && (tmp->parent->left == NULL ||
+		(tmp->parent->left && tmp->parent->left->color == BLACK)))) {
+			std::cout << "LEFT: " << tmp->data << " SUN: " << child->data << std::endl;
+			if (tmp->left == child) {
+				rightRotateSmall(tmp, child);
+				leftRotate(child);
+			}
+			else {
+				leftRotate(tmp);
+			}
+			
+		}
+		else if (tmp->parent != NULL && (tmp->parent->left == tmp && (tmp->parent->right == NULL ||
+		(tmp->parent->right && tmp->parent->right->color == BLACK)))) {
+			std::cout << "RIGHT: " << tmp->data << " SUN: " << child->data << std::endl;
+			if (tmp->right == child) {
+				leftRotateSmall(tmp, child);
+				rightRotate(child);
+			}
+			else {
+				rightRotate(tmp);
+			}
+		}
+	}
+
+
+	void findPlaceRecursive(const T &data, Tree<T> *tmp, Tree<T> *newTree) {
+		if (data > tmp->data) {
+			if (tmp->right != NULL) {
+				findPlaceRecursive(data, tmp->right, newTree);
+			}
+			else {
+				//std::cout << "add: " << data << std::endl;
+				tmp->right = newTree;
+				newTree->parent = tmp;
+				redUncle(tmp);
+				if (tmp->parent != NULL && tmp->color == RED) {
+					blackUncle(tmp, newTree);
+				}
+				return ;
+			}
+		}
+		else if (data < tmp->data) {
+			if (tmp->left != NULL) {
+				findPlaceRecursive(data, tmp->left, newTree);
+			}
+			else {
+				//std::cout << "add: " << data << std::endl;
+				tmp->left = newTree;
+				newTree->parent = tmp;
+				redUncle(tmp);
+				if (tmp->parent != NULL && tmp->color == RED) {
+					blackUncle(tmp, newTree);
+				}
+				return ;
+			}
+		}
+		else {
+				//std::cout << "delete: " << data << std::endl;
+				delete newTree;
+				return ;
+		}
+	}
+
+
+	Tree<T> *newNode(const T &data) {
+		Tree<T> *newTree = new Tree<T>;
+		newTree->data = data;
+		newTree->color = RED;
+		newTree->left = NULL;
+		newTree->right = NULL;
+		newTree->parent = NULL;
+		this->count++;
+		return newTree;
+	}
+
+	    void leftRotate(Tree<T> *rotate) {
+        std::cout << "leftRotate: " << rotate->data << std::endl;
         Tree<T> *tmp = rotate->parent;
         if (rotate->parent->parent != NULL) {
             rotate->parent = rotate->parent->parent; //1
@@ -129,14 +227,14 @@ class redBlackTree {
                 rotate->left = tmp;
             }
             this->tree = rotate;
-            this->tree->color = Black;
+            this->tree->color = BLACK;
         }
-        tmp->color = Red;
-        rotate->color = Black;
+        tmp->color = RED;
+        rotate->color = BLACK;
     }
-    
+
     void rightRotate(Tree<T> *rotate) {
-        std::cout << "rightRotate" << std::endl;
+		std::cout << "rightRotate: " << rotate->data << std::endl;
         Tree<T> *tmp = rotate->parent;
         if (rotate->parent->parent != NULL) {
             rotate->parent = rotate->parent->parent;
@@ -165,16 +263,29 @@ class redBlackTree {
                 rotate->right = tmp;
             }
             this->tree = rotate;
-            this->tree->color = Black;
+            this->tree->color = BLACK;
         }
-        tmp->color = Red;
-        rotate->color = Black;
+        tmp->color = RED;
+        rotate->color = BLACK;
     }
-    
-    Tree<T> *getTree() {
-        return tree;
-    }
-    
+
+
+	void insert(const T &data) {
+		if (tree == NULL) {
+			tree = newNode(data);
+			tree->color = BLACK;
+		}
+		else {
+			Tree<T> *newTree = newNode(data);
+			Tree<T> *tmp = tree;
+			findPlaceRecursive(data, tmp, newTree);
+			//findPlace(data, tmp, newTree);
+		}
+	}
+
+	void remove(const T &data) {
+
+	}
 };
 
 #endif
